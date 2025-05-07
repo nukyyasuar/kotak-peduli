2c81a724136a94973f15605ff588a93640132e7e
Nuky Yasuar Zamzamy <intern@netmonk.id>
2025-05-07 19:24:50 +0700

    feat: add outlineBrown variant, property newKey

--- src/components/button.jsx
+++ src/components/button.jsx
@@ -1,13 +1,15 @@
 import { Icon } from "@iconify/react";
 
 const baseClass =
-  "flex-1 text-base py-2 rounded-lg font-bold px-7 flex items-center justify-center gap-1";
+  "text-base py-2 rounded-lg font-bold px-7 flex items-center justify-center gap-1";
 const variantStyles = {
   white: "bg-white text-[#543A14] border border-[#543A14]hover:text-white",
-  brown: "bg-[#543A14] text-white hover:bg-[#6B4D20] hover:text-white",
+  brown: "bg-[#543a14] text-white hover:bg-[#6B4D20] hover:text-white",
   orange: "bg-[#F0BB78] text-white hover:bg-[#E09359]",
   outlineOrange:
     "border border-[#F0BB78] text-black hover:bg-[#F0BB78] hover:text-white",
+  outlineBrown:
+    "border border-[#543A14] text-black hover:bg-[#543a14] hover:text-white",
 };
 
 const ButtonCustom = ({
@@ -17,13 +19,13 @@ const ButtonCustom = ({
   icon,
   className,
   type,
-  key,
+  newKey,
   disabled,
 }) => {
   return (
     <button
       type={type}
-      key={key}
+      key={newKey}
       className={`${baseClass} ${variantStyles[variant]} ${className} ${disabled ? `cursor-not-allowed` : `cursor-pointer`}`}
       onClick={onClick}
       disabled={disabled}
