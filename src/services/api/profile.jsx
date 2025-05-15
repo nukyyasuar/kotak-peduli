7d744747e9b86ad04a33e74cb5eb5bff54b9bd67
Nuky Yasuar Zamzamy <intern@netmonk.id>
2025-05-15 16:43:20 +0700

    feat: refactor updateProfile function to remove userId parameter and add verifyOtp function

--- src/services/api/profile.jsx
+++ src/services/api/profile.jsx
@@ -18,10 +18,10 @@ const getProfile = async () => {
   }
 };
 
-const updateProfile = async (userId, payload) => {
+const updateProfile = async (payload) => {
   try {
     const response = await fetchWithAuth(
-      `${process.env.NEXT_PUBLIC_DOMAIN}/users/${userId}`,
+      `${process.env.NEXT_PUBLIC_DOMAIN}/users`,
       createRequestOptions("PATCH", payload)
     );
     const result = await handleApiResponse(response);
@@ -32,4 +32,32 @@ const updateProfile = async (userId, payload) => {
   }
 };
 
-export { getProfile, updateProfile };
+const updateAvatar = async (payload) => {
+  try {
+    const response = await fetchWithAuth(
+      `${process.env.NEXT_PUBLIC_DOMAIN}/users/profile/avatar`,
+      createRequestOptions("POST", payload)
+    );
+    const result = await handleApiResponse(response);
+
+    return result.data;
+  } catch (error) {
+    throw new Error(error.message);
+  }
+};
+
+const verifyOtp = async (idToken) => {
+  try {
+    const response = await fetchWithAuth(
+      `${process.env.NEXT_PUBLIC_DOMAIN}/auth/verify-otp`,
+      createRequestOptions("POST", { idToken })
+    );
+    const result = await handleApiResponse(response);
+
+    return result.data;
+  } catch (error) {
+    throw new Error(error.message);
+  }
+};
+
+export { getProfile, updateProfile, updateAvatar, verifyOtp };
