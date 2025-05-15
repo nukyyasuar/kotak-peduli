a5128e70238c3daca79c5657f2af83de39b32589
Nuky Yasuar Zamzamy <intern@netmonk.id>
2025-05-15 16:43:28 +0700

    feat: refactor ListTextWithTitle component to accept className prop and improve styling; add Spacer and TextWithLink components for better layout

--- src/components/text.jsx
+++ src/components/text.jsx
@@ -1,11 +1,12 @@
 import { Icon } from "@iconify/react";
 import Image from "next/image";
+import Link from "next/link";
 
 const IconText = ({ text, src, alt, iconType, variant }) => {
   return (
     <div className="flex gap-2 items-center justify-center">
       <div
-        className={`h-8 aspect-square rounded-full bg-${variant === "white" ? "[#543A14]" : "white"} flex items-center justify-center`}
+        className={`h-8 aspect-square rounded-full ${variant === "white" ? "bg-[#543A14]" : "bg-white"} flex items-center justify-center`}
       >
         {iconType === "img" ? (
           <Image src={src} alt={alt} width={20} height={20} />
@@ -49,9 +50,9 @@ const TextBetween = ({ label, value, className, type }) => {
   );
 };
 
-const ListTextWithTitle = ({ title, values }) => {
+const ListTextWithTitle = ({ title, values, className }) => {
   return (
-    <div className="flex flex-col gap-1">
+    <div className={`flex flex-col gap-1 ${className}`}>
       <span className="font-bold">{title}</span>
       {values.map((value, idx) => (
         <span key={idx}>{value}</span>
@@ -60,4 +61,30 @@ const ListTextWithTitle = ({ title, values }) => {
   );
 };
 
-export { IconText, TextBetween, ListTextWithTitle };
+const Spacer = ({ text }) => {
+  return (
+    <div className="flex items-center">
+      <div className="flex-1 border-t border-[#C2C2C2]" />
+      <span className="px-3 text-[#C2C2C2] text-sm">{text}</span>
+      <div className="flex-1 border-t border-[#C2C2C2]" />
+    </div>
+  );
+};
+
+const TextWithLink = ({ text, label, href }) => {
+  return (
+    <div className="text-center">
+      <span>
+        {text}{" "}
+        <Link
+          href={href}
+          className="hover:underline cursor-pointer font-bold text-[#F0BB78]"
+        >
+          {label}
+        </Link>
+      </span>
+    </div>
+  );
+};
+
+export { IconText, TextBetween, ListTextWithTitle, Spacer, TextWithLink };
