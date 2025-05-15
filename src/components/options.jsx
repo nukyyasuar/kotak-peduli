07cbb55c796a3acccf5057cb7cefb9f05420a5cb
Nuky Yasuar Zamzamy <intern@netmonk.id>
2025-05-15 08:05:07 +0700

    feat: options component

---
+++ src/components/options.jsx
@@ -0,0 +1,42 @@
+const statusList = [
+  { label: "Semua", value: "" },
+  { label: "Pemeriksaan Digital", value: "DIGITAL_CHECKING" },
+  { label: "Pengiriman", value: "TRANSPORTING" },
+  { label: "Penjemputan", value: "penjemputan" },
+  { label: "Dalam Perjalanan", value: "IN_TRANSIT" },
+  { label: "Pemeriksaan Fisik", value: "PHYSICAL_CHECKING" },
+  { label: "Disimpan", value: "STORED" },
+  { label: "Dialihkan", value: "REDIRECTED" },
+  { label: "Disalurkan", value: "DISTRIBUTED" },
+];
+
+const donationTypes = [
+  { label: "Pakaian", value: "CLOTHES" },
+  { label: "Alat Elektronik", value: "ELECTRONICS" },
+  { label: "Buku", value: "BOOKS" },
+  { label: "Mainan", value: "TOYS" },
+];
+
+const shippingTypes = [
+  { label: "Dikirim Sendiri", value: "DELIVERED" },
+  { label: "Dijemput", value: "PICKED_UP" },
+];
+
+const electronicOptions = [
+  { label: "Laptop", value: "laptop" },
+  { label: "Televisi", value: "tv" },
+  { label: "Kulkas", value: "kulkas" },
+  { label: "Mesin Cuci", value: "mesin_cuci" },
+  { label: "Microwave", value: "microwave" },
+  { label: "AC", value: "ac" },
+  { label: "Kipas Angin", value: "kipas_angin" },
+  { label: "Setrika", value: "setrika" },
+  { label: "Blender", value: "blender" },
+  { label: "Rice Cooker", value: "rice_cooker" },
+  { label: "Speaker", value: "speaker" },
+  { label: "Printer", value: "printer" },
+  { label: "Monitor", value: "monitor" },
+  { label: "Proyektor", value: "proyektor" },
+];
+
+export { statusList, donationTypes, shippingTypes, electronicOptions };
