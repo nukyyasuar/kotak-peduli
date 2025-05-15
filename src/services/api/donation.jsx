cec110bf2344a1a680c70c0ebd4ac7b84ee472a0
Nuky Yasuar Zamzamy <intern@netmonk.id>
2025-05-15 16:43:10 +0700

    feat: add donation retrieval and attachment fetching functions

--- src/services/api/donation.jsx
+++ src/services/api/donation.jsx
@@ -1,8 +1,11 @@
+"use client";
+
 import {
   createRequestOptions,
   handleApiResponse,
   fetchWithAuth,
 } from "../helpers";
+// const accessToken = localStorage.getItem("authToken");
 
 const createDonation = async (payload) => {
   try {
@@ -18,4 +21,51 @@ const createDonation = async (payload) => {
   }
 };
 
-export { createDonation };
+const getDonations = async () => {
+  try {
+    const response = await fetchWithAuth(
+      `${process.env.NEXT_PUBLIC_DOMAIN}/users/profile/donations`,
+      createRequestOptions("GET")
+    );
+    const result = await handleApiResponse(response);
+
+    return result.data;
+  } catch (error) {
+    throw new Error(error.message);
+  }
+};
+
+const getOneDonation = async (id) => {
+  try {
+    const response = await fetchWithAuth(
+      `${process.env.NEXT_PUBLIC_DOMAIN}/donation-items/${id}`,
+      createRequestOptions("GET")
+    );
+    const result = await handleApiResponse(response);
+
+    return result.data;
+  } catch (error) {
+    throw new Error(error.message);
+  }
+};
+
+const getAttachment = async (fileName) => {
+  try {
+    const response = await fetchWithAuth(
+      `${process.env.NEXT_PUBLIC_DOMAIN}/uploads/${fileName}`,
+      createRequestOptions("GET")
+    );
+
+    if (!response.ok) {
+      throw new Error("Failed to fetch image");
+    }
+
+    const blob = await response.blob();
+    return blob;
+  } catch (error) {
+    console.error("Attachment fetch error:", error);
+    throw error;
+  }
+};
+
+export { createDonation, getDonations, getOneDonation, getAttachment };
