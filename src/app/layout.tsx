4fdc11b499d6bb0626c90f73fd67e696afbd3c04
Nuky Yasuar Zamzamy <intern@netmonk.id>
2025-05-15 16:45:12 +0700

    feat: update RootLayout to conditionally render Header and Footer based on pathname

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,9 @@
+"use client";
+
+import { usePathname } from "next/navigation";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
 import "./globals.css";
 import Header from "src/components/layout/header";
 import Footer from "src/components/layout/footer";
@@ -7,12 +13,20 @@ export default function RootLayout({
 }: {
   children: React.ReactNode;
 }) {
+  const pathname = usePathname();
+  const showLayout = pathname !== "/login" && pathname !== "/daftar";
+
   return (
     <html lang="id">
       <body>
-        <Header />
-        <div className="min-h-[82dvh] mt-20">{children}</div>
-        <Footer />
+        {showLayout && <Header />}
+
+        <div className={`min-h-[82dvh] ${showLayout && "mt-20"}`}>
+          {children}
+        </div>
+        <ToastContainer position="top-right" autoClose={2000} />
+
+        {showLayout && <Footer />}
       </body>
     </html>
   );
