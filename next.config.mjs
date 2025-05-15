ee152a48fd5a881e5165f7998efc5e4281f440e9
Nuky Yasuar Zamzamy <intern@netmonk.id>
2025-05-15 16:35:30 +0700

    feat: add protocol and hostname for domain, so next/image can read it

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "clear-mammal-simply.ngrok-free.app",
+        // pathname: '/account123/**',
+      },
+    ],
+  },
+};
 
 export default nextConfig;
