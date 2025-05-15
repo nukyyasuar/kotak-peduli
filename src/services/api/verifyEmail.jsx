ca9abcf9df21657ba84dd776eebe6e422d897b26
Nuky Yasuar Zamzamy <intern@netmonk.id>
2025-05-15 16:27:10 +0700

    feat: add verify email and confirm email endpoint file

---
+++ src/services/api/verifyEmail.jsx
@@ -0,0 +1,35 @@
+import {
+  createRequestOptions,
+  handleApiResponse,
+  fetchWithAuth,
+} from "../helpers";
+
+const verifyEmail = async () => {
+  try {
+    const response = await fetchWithAuth(
+      `${process.env.NEXT_PUBLIC_DOMAIN}/auth/verify-email`,
+      createRequestOptions("POST")
+    );
+    const result = await handleApiResponse(response);
+
+    return result.data;
+  } catch (error) {
+    throw new Error(error.message);
+  }
+};
+
+const confirmEmail = async (token) => {
+  try {
+    const response = await fetchWithAuth(
+      `${process.env.NEXT_PUBLIC_DOMAIN}/auth/confirm-email`,
+      createRequestOptions("POST", token)
+    );
+    const result = await handleApiResponse(response);
+    console.log(result);
+    return result.data;
+  } catch (error) {
+    throw new Error(error.message);
+  }
+};
+
+export { verifyEmail, confirmEmail };
