c340f05e1dc06b363012616b87cdc426c06b8489
Nuky Yasuar Zamzamy <intern@netmonk.id>
2025-05-21 18:10:49 +0700

    feat: add getCollectionCenterDonations function with filtering options

--- src/services/api/collectionCenter.jsx
+++ src/services/api/collectionCenter.jsx
@@ -46,4 +46,68 @@ const createCollectionCenter = async (payload) => {
   }
 };
 
-export { getCollectionCenters, getOneCollectionCenter, createCollectionCenter };
+const getCollectionCenterDonations = async (
+  id,
+  page,
+  keyword,
+  statusFilters,
+  donationTypesFilters,
+  pickupFilters
+) => {
+  try {
+    const params = new URLSearchParams({
+      page: page.toString(),
+      limit: "10",
+      sort: "createdAt:desc",
+    });
+
+    const appendMultipleFilters = (params, key, values) => {
+      values.forEach((val) => params.append(key, val));
+    };
+
+    if (keyword) {
+      params.append("search", keyword);
+    }
+    if (statusFilters.length > 0) {
+      appendMultipleFilters(params, "statusTypes", statusFilters);
+    }
+    if (donationTypesFilters.length > 0) {
+      appendMultipleFilters(params, "donationTypes", donationTypesFilters);
+    }
+    if (pickupFilters.length > 0) {
+      appendMultipleFilters(params, "pickupTypes", pickupFilters);
+    }
+
+    const response = await fetchWithAuth(
+      `${process.env.NEXT_PUBLIC_DOMAIN}/collection-centers/${id}/donation-items?${params.toString()}`,
+      createRequestOptions("GET")
+    );
+    const result = await handleApiResponse(response);
+
+    return result;
+  } catch (error) {
+    throw new Error(error.message);
+  }
+};
+
+const updateCollectionCenter = async (collectionCenterid, payload) => {
+  try {
+    const response = await fetchWithAuth(
+      `${process.env.NEXT_PUBLIC_DOMAIN}/collection-centers/${collectionCenterid}`,
+      createRequestOptions("PATCH", payload)
+    );
+    const result = await handleApiResponse(response);
+
+    return result.data;
+  } catch (error) {
+    throw new Error(error.message);
+  }
+};
+
+export {
+  getCollectionCenters,
+  getOneCollectionCenter,
+  createCollectionCenter,
+  getCollectionCenterDonations,
+  updateCollectionCenter,
+};
