ab4f969080e10ea30ff83d88aef74c7aecef8055
Nuky Yasuar Zamzamy <intern@netmonk.id>
2025-05-21 18:11:09 +0700

    feat: add getEventsWithParams, createEventCollectionCenter, and updateEventCollectionCenter functions for enhanced event management

--- src/services/api/event.jsx
+++ src/services/api/event.jsx
@@ -18,4 +18,73 @@ const getEvents = async (collectionCenterId) => {
   }
 };
 
-export { getEvents };
+const getEventsWithParams = async (
+  collectionCenterId,
+  page,
+  keyword,
+  statusFilters
+) => {
+  try {
+    const params = new URLSearchParams({
+      page: page.toString(),
+      limit: "2",
+      sort: "endDate:asc",
+    });
+
+    if (keyword) {
+      params.append("search", keyword);
+    }
+    if (statusFilters.length === 1) {
+      params.append("isActive", statusFilters);
+    }
+
+    const response = await fetchWithAuth(
+      `${process.env.NEXT_PUBLIC_DOMAIN}/collection-centers/${collectionCenterId}/events?${params.toString()}`,
+      createRequestOptions("GET")
+    );
+    const result = await handleApiResponse(response);
+
+    return result;
+  } catch (error) {
+    throw new Error(error.message);
+  }
+};
+
+const createEventCollectionCenter = async (collectionCenterId, payload) => {
+  try {
+    const response = await fetchWithAuth(
+      `${process.env.NEXT_PUBLIC_DOMAIN}/collection-centers/${collectionCenterId}/events`,
+      createRequestOptions("POST", payload)
+    );
+    const result = await handleApiResponse(response);
+
+    return result;
+  } catch (error) {
+    throw new Error(error.message);
+  }
+};
+
+const updateEventCollectionCenter = async (
+  collectionCenterId,
+  eventId,
+  payload
+) => {
+  try {
+    const response = await fetchWithAuth(
+      `${process.env.NEXT_PUBLIC_DOMAIN}/collection-centers/${collectionCenterId}/events/${eventId}`,
+      createRequestOptions("PATCH", payload)
+    );
+    const result = await handleApiResponse(response);
+
+    return result;
+  } catch (error) {
+    throw new Error(error.message);
+  }
+};
+
+export {
+  getEvents,
+  getEventsWithParams,
+  createEventCollectionCenter,
+  updateEventCollectionCenter,
+};
