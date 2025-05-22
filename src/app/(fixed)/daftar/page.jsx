c409246b12914980a9868c30c8cb37793b703648
Nuky Yasuar Zamzamy <intern@netmonk.id>
2025-05-22 16:08:45 +0700

    feat: error message and loading handling

--- src/app/(fixed)/daftar/page.jsx
+++ src/app/(fixed)/daftar/page.jsx
@@ -7,6 +7,7 @@ import { useRouter } from "next/navigation";
 import { useForm } from "react-hook-form";
 import { yupResolver } from "@hookform/resolvers/yup";
 import { toast } from "react-toastify";
+import { ClipLoader } from "react-spinners";
 
 import { registrationSchema } from "src/components/schema/registrationSchema";
 import { FormInput } from "src/components/formInput";
@@ -23,7 +24,6 @@ import {
   verifyPhoneCode,
 } from "../../../services/auth/auth";
 import {
-  setConfirmationResult,
   getConfirmationResult,
   clearConfirmationResult,
 } from "../../../services/auth/authStore";
@@ -32,6 +32,7 @@ export default function Registration() {
   // Form/Registration states
   const [user, setUser] = useState(null);
   const [isLoading, setIsLoading] = useState(false);
+  const [isLoadingSubmit, setIsLoadingSubmit] = useState(false);
   const router = useRouter();
 
   // Add new state to control view
@@ -82,7 +83,9 @@ export default function Registration() {
   };
 
   const onSubmit = async (data) => {
-    setIsLoading(true);
+    console.log("data", data);
+    setIsLoadingSubmit(true);
+
     try {
       const formattedPhoneNumber = "+62" + data.phoneNumber;
       const registrationData = { ...data, phoneNumber: formattedPhoneNumber };
@@ -143,7 +146,7 @@ export default function Registration() {
       toast.error(
         err.message || "Terjadi kesalahan saat memproses pendaftaran"
       );
-      setIsLoading(false);
+      setIsLoadingSubmit(false);
     }
   };
 
@@ -233,20 +236,23 @@ export default function Registration() {
       router.push("/login");
     } catch (err) {
       console.error("Error during OTP verification or registration:", err);
-      {
-        err?.code === "auth/invalid-verification-code"
-          ? toast.error("Kode OTP salah. Silakan coba lagi.")
-          : err?.code === "auth/session-expired"
-            ? toast.error(
-                "Sesi OTP telah kedaluwarsa. Silakan minta kode baru."
-              )
-            : err?.message?.includes("Data registrasi tidak lengkap")
-              ? toast.error(err.message)
-              : toast.error(
-                  `Gagal memverifikasi OTP: ${err.message || "Terjadi kesalahan"}`
-                );
+      if (err?.code === "auth/invalid-verification-code") {
+        toast.error("Kode OTP salah. Silakan coba lagi.");
       }
-
+      if (err?.code === "auth/session-expired") {
+        toast.error("Sesi OTP telah kedaluwarsa. Silakan minta kode baru.");
+      }
+      if (err?.code === "auth/too-many-requests") {
+        toast.error("Terlalu banyak permintaan. Silakan coba lagi nanti.");
+      }
+      if (
+        err?.message ===
+        "A user with the same email or phone number already exists"
+      ) {
+        toast.error(
+          "Akun dengan nomor telepon ini sudah terdaftar. Silakan gunakan nomor lain."
+        );
+      }
       if (
         err?.message?.includes("tidak lengkap") ||
         err?.code === "auth/invalid-phone-number"
@@ -260,10 +266,10 @@ export default function Registration() {
   };
 
   const handleResendOtp = async (dataToUse = null) => {
-    const data = dataToUse || registrationData;
     setIsResendLoading(true);
 
-    const phoneNumber = data?.phoneNumber;
+    const phoneNumber = "+62" + watch("phoneNumber");
+
     if (!phoneNumber) {
       setIsResendLoading(false);
       setShowOtpVerification(false);
@@ -290,9 +296,7 @@ export default function Registration() {
       setIsTimerRunning(true);
       setOtp(["", "", "", "", "", ""]);
 
-      if (!dataToUse) {
-        toast.success("Kode OTP baru telah dikirim.");
-      }
+      toast.success("Kode OTP baru telah dikirim.");
     } catch (err) {
       console.error("Error while resending OTP:", err);
 
@@ -306,6 +310,8 @@ export default function Registration() {
           setConfirmationResult(confirmation);
           setTimer(60);
           setIsTimerRunning(true);
+
+          toast.success("Kode OTP baru telah dikirim.");
           return;
         } catch (innerErr) {
           console.error("Gagal recover dari reCAPTCHA error:", innerErr);
@@ -532,26 +538,12 @@ export default function Registration() {
                   label={[
                     isLoading ? (
                       <>
-                        <svg
-                          className="animate-spin h-5 w-5 mr-2 text-white"
-                          xmlns="http://www.w3.org/2000/svg"
-                          fill="none"
-                          viewBox="0 0 24 24"
-                        >
-                          <circle
-                            className="opacity-25"
-                            cx="12"
-                            cy="12"
-                            r="10"
-                            stroke="currentColor"
-                            strokeWidth="4"
-                          ></circle>
-                          <path
-                            className="opacity-75"
-                            fill="currentColor"
-                            d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
-                          ></path>
-                        </svg>
+                        <ClipLoader
+                          color="white"
+                          size={20}
+                          className="mr-2"
+                          loading={isLoading}
+                        />
                         Memproses...
                       </>
                     ) : (
