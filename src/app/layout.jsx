d3d57cc975b52607b2d24a0a5c6303149cbd75ba
Nuky Yasuar Zamzamy <intern@netmonk.id>
2025-05-24 11:14:57 +0700

    feat: add RootLayout component for main application structure

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -7,26 +7,26 @@ import "react-toastify/dist/ReactToastify.css";
 import "./globals.css";
 import Header from "src/components/layout/header";
 import Footer from "src/components/layout/footer";
+import { AuthProvider } from "src/services/auth/AuthContext";
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+export default function RootLayout({ children }) {
   const pathname = usePathname();
   const showLayout = pathname !== "/login" && pathname !== "/daftar";
 
   return (
     <html lang="id">
       <body>
-        {showLayout && <Header />}
+        <AuthProvider>
+          {showLayout && <Header />}
 
-        <div className={`min-h-[82dvh] ${showLayout && "mt-20"}`}>
-          {children}
-        </div>
-        <ToastContainer position="top-right" autoClose={2000} />
+          <div className={`min-h-[80dvh] ${showLayout && "mt-20"}`}>
+            {children}
+          </div>
 
-        {showLayout && <Footer />}
+          <ToastContainer position="top-right" autoClose={2000} />
+
+          {showLayout && <Footer />}
+        </AuthProvider>
       </body>
     </html>
   );
