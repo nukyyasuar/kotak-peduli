07b2974301f5a83b7764dfec400042356a4086b6
Nuky Yasuar Zamzamy <nukyyasuarzamzamy@gmail.com>
2025-06-05 16:57:01 +0700

    fix: reset open menu index when opening add event modal to improve UI behavior

--- src/app/(fixed)/admin/event/page.jsx
+++ src/app/(fixed)/admin/event/page.jsx
@@ -314,6 +314,7 @@ export default function CollectionCenterEvents() {
                 icon="material-symbols:add"
                 onClick={() => {
                   setIsAddEventModalOpen(true);
+                  setOpenMenuIndex(null);
                 }}
               />
             </div>
