49db115f22df8a2bedbf9100452c1ee976fa1416
Nuky Yasuar Zamzamy <nukyyasuarzamzamy@gmail.com>
2025-06-05 16:33:20 +0700

    fix: enhance form submission handling and improve error display

--- src/app/(fixed)/admin/testimoni/page.jsx
+++ src/app/(fixed)/admin/testimoni/page.jsx
@@ -79,94 +79,96 @@ export default function Testimony() {
             UCAPAN TERIMA KASIH
           </h1>
 
-          <form
-            onSubmit={handleSubmit(onSubmit)}
-            className="w-xl bg-[#F5E9D4] p-6 shadow-lg rounded-lg space-y-3 text-black"
-          >
-            <FormInput
-              inputType="text"
-              label="Nama Penerima"
-              placeholder="Contoh: Matthew"
-              register={register("name")}
-              required
-              errors={errors?.name?.message}
-              inputStyles="bg-white"
-            />
-            <FormInput
-              inputType="text"
-              label="Jabatan atau Peran"
-              placeholder="Contoh: Ketua Yayasan"
-              register={register("title")}
-              required
-              errors={errors?.title?.message}
-              inputStyles="bg-white"
-            />
-            <FormInput
-              inputType="textArea"
-              label="Ucapan atau Pesan"
-              placeholder="Contoh: Terima kasih atas bantuan yang diberikan"
-              register={register("message")}
-              required
-              errors={errors?.message?.message}
-              inputStyles="bg-white"
-            />
-            <div className="w-full">
-              <div className="flex items-end gap-3">
-                <FormInput
-                  inputType="text"
-                  value={watch("file")?.name}
-                  register={register("file")}
-                  label="Foto"
-                  inputStyles="bg-white relative min-h-[3rem] flex items-center gap-2 px-2 py-1 border rounded-lg max-w-[461px] overflow-scroll"
-                  required
-                  className="w-full"
-                />
-                <div className="flex">
-                  <label
-                    htmlFor="file"
-                    className="px-4 py-3 bg-[#F0BB78] text-nowrap rounded-lg font-semibold text-white cursor-pointer"
-                  >
-                    Pilih File
-                  </label>
-                  <input
-                    id="file"
-                    type="file"
-                    accept="image/*"
-                    className="hidden"
-                    onChange={(e) => {
-                      const file = e.target.files[0];
-                      setValue("file", file);
-                    }}
+          <fieldset disabled={isLoadingSubmit}>
+            <form
+              onSubmit={handleSubmit(onSubmit)}
+              className="w-xl bg-[#F5E9D4] p-6 shadow-lg rounded-lg space-y-3 text-black"
+            >
+              <FormInput
+                inputType="text"
+                label="Nama Penerima"
+                placeholder="Contoh: Matthew"
+                register={register("name")}
+                required
+                errors={errors?.name?.message}
+                inputStyles="bg-white"
+              />
+              <FormInput
+                inputType="text"
+                label="Jabatan atau Peran"
+                placeholder="Contoh: Ketua Yayasan"
+                register={register("title")}
+                required
+                errors={errors?.title?.message}
+                inputStyles="bg-white"
+              />
+              <FormInput
+                inputType="textArea"
+                label="Ucapan atau Pesan"
+                placeholder="Contoh: Terima kasih atas bantuan yang diberikan"
+                register={register("message")}
+                required
+                errors={errors?.message?.message}
+                inputStyles="bg-white"
+              />
+              <div className="w-full">
+                <div className="flex items-end gap-3">
+                  <FormInput
+                    inputType="text"
+                    value={watch("file")?.name}
+                    register={register("file")}
+                    label="Foto"
+                    inputStyles="bg-white relative min-h-[3rem] flex items-center gap-2 px-2 py-1 border rounded-lg max-w-[461px] overflow-scroll"
+                    required
+                    className="w-full"
                   />
+                  <div className="flex">
+                    <label
+                      htmlFor="file"
+                      className="px-4 py-3 bg-[#F0BB78] text-nowrap rounded-lg font-semibold text-white cursor-pointer"
+                    >
+                      Pilih File
+                    </label>
+                    <input
+                      id="file"
+                      type="file"
+                      accept="image/*"
+                      className="hidden"
+                      onChange={(e) => {
+                        const file = e.target.files[0];
+                        setValue("file", file);
+                      }}
+                    />
+                  </div>
                 </div>
+                {errors?.file?.message && (
+                  <p className="text-[#E52020] text-sm mt-1">
+                    {errors?.file?.message}
+                  </p>
+                )}
               </div>
-              {errors?.file?.message && (
-                <p className="text-[#E52020] text-sm mt-1">
-                  {errors?.file?.message}
-                </p>
-              )}
-            </div>
 
-            <ButtonCustom
-              type="submit"
-              variant="brown"
-              label={
-                isLoadingSubmit ? (
-                  <ClipLoader
-                    color="white"
-                    loading={isLoadingSubmit}
-                    size={20}
-                    aria-label="Loading Spinner"
-                    data-testid="loader"
-                  />
-                ) : (
-                  "Kirim Ucapan"
-                )
-              }
-              className="w-full h-12 mt-6"
-              disabled={isLoadingSubmit}
-            />
-          </form>
+              <ButtonCustom
+                type="submit"
+                variant="brown"
+                label={
+                  isLoadingSubmit ? (
+                    <ClipLoader
+                      color="white"
+                      loading={isLoadingSubmit}
+                      size={20}
+                      aria-label="Loading Spinner"
+                      data-testid="loader"
+                    />
+                  ) : (
+                    "Kirim Ucapan"
+                  )
+                }
+                className="w-full h-12 mt-6"
+                disabled={isLoadingSubmit}
+              />
+            </form>
+          </fieldset>
         </div>
       )}
     </div>
