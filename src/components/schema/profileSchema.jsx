da687064bfd7345fbd0c7ee52185e847adfb6289
Nuky Yasuar Zamzamy <nukyyasuarzamzamy@gmail.com>
2025-06-17 05:45:45 +0700

    fix: update phone number validation regex to allow 8-14 digits

--- src/components/schema/profileSchema.jsx
+++ src/components/schema/profileSchema.jsx
@@ -26,7 +26,7 @@ const profileSchema = yup.object().shape({
     .min(9, "Nomor telepon minimal terdiri dari 9 digit.")
     .max(15, "Nomor telepon maksimal terdiri dari 15 digit.")
     .matches(
-      /^8\d{9,14}$/,
+      /^8\d{8,14}$/,
       "Nomor telepon harus dimulai dengan angka ‘8’ (contoh: 81231231231)"
     ),
   email: yup
